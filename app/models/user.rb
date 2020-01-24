6370e7702249f51567aeede67c430a12bdb98863
Volovenko <vadim.volovenko@gmail.com>
2020-01-24 11:26:43 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,13 @@ require 'openssl'
 class User < ApplicationRecord
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
-  VALID_EMAIL_REGEX = /[a-zA-Z0-9_]i/
 
   attr_accessor :password
 
   has_many :questions
 
   validates :email, :username, presence: true
-  validates :username, length: { maximum: 40 }, format: { with: VALID_EMAIL_REGEX }
+  validates :username, length: { maximum: 40 }, format: { with: /[a-zA-Z0-9_]i/}
   validates :email, :username, uniqueness: true
   validates :email, email: true
   validates :password, presence: true, on: :create
