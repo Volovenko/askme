5d34ad33b27450bb02974df7ee9eddd55b60bb8b
Volovenko <vadim.volovenko@gmail.com>
2020-01-28 16:37:03 +0300

    add questions number and inclination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,21 @@ module ApplicationHelper
       asset_path 'avatar.png'
     end
   end
+
+  def inclination(number, vopros, voprosa, voprosov)
+    sklonenie = number % 10
+    big_sklonenie = number % 100
+
+    if number == 0 || !number.is_a?(Numeric)
+      voprosov
+    elsif big_sklonenie.between?(11, 14)
+      voprosov
+    elsif sklonenie == 1
+      vopros
+    elsif sklonenie.between?(2, 4)
+      voprosa
+    else
+      voprosov
+    end
+  end
 end
