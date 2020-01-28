8419bb66ebcb04a229ef83615826b2feb51dce2c
Volovenko <vadim.volovenko@gmail.com>
2020-01-28 14:53:16 +0300

    add before_validation and index for username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_100945) do
+ActiveRecord::Schema.define(version: 2020_01_28_111325) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
