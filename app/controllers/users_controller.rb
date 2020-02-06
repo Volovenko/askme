989cd52ce176a08f049060ec63d17b35147197f5
Volovenko <vadim.volovenko@gmail.com>
2020-02-06 16:56:16 +0300

    add auth users after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_url, notice: "Отлично, Вы зарегистрировались!"
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "Отлично, Вы зарегистрировались и залогинились!"
     else
       render 'new'
 
